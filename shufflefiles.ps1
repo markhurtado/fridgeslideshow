7ea7a8b5cb05f52dbbac5d676bda192852d53b9d
markhurtado <markhurtado89@gmail.com>
2023-07-24 17:21:09 -0600

    Delete old code

--- shufflefiles.ps1
+++ shufflefiles.ps1
@@ -5,21 +5,6 @@ $destinationFolderPath = "D:"
 # Get the list of files in the source folder using case-insensitive regex filter
 $files = Get-ChildItem -Path $sourceFolderPath | Where-Object { $_.Name -match '(?i)\.jpg$' }
 
-
-# Change the file extensions in the source folder to lowercase
-# foreach ($file in $files) {
-#     $extension = $file.Extension
-#     if ($extension -ne $extension.ToLower()) {
-#         $newPath = $file.FullName.Replace($extension, $extension.ToLower())
-#         Rename-Item -Path $file.FullName -NewName $newPath -ErrorAction SilentlyContinue
-#     }
-# }
-
-# # Ignore the files named 01.jpg through 24.jpg
-# $ignoredFileList = @("01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg", "06.jpg", "07.jpg", "08.jpg", "09.jpg", "10.jpg", "11.jpg", "12.jpg", "13.jpg", "14.jpg", "15.jpg", "16.jpg", "17.jpg", "18.jpg", "19.jpg", "20.jpg", "21.jpg", "22.jpg", "23.jpg", "24.jpg")
-# $ignoredFiles = $files | Where-Object { $ignoredFileList -contains $_.Name }
-# $filteredFiles = $files | Where-Object { $ignoredFileList -notcontains $_.Name }
-
 # Define the regex pattern to match the ignored files (01.jpg through 24.jpg)
 $ignoredFilesPattern = "(?i)^(0[1-9]|1[0-9]|2[0-4])\.jpg$"
 $ignoredFilesRegex = [regex]::new($ignoredFilesPattern)
